42a466cde3e72320cba9aa5ae6e4baeb933167f9
Seth Roberts <sar242004@gmail.com>
2025-04-01 18:30:49 -0500

    Add project files.

---
+++ Study 13/Study13.cpp
@@ -0,0 +1,12 @@
+#include <WinSock2.h>
+#include <ws2tcpip.h>
+#include <iostream>
+#include <string>
+#include "StudyBuddy.h"
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+#pragma comment(lib, "Ws2_32.lib")
